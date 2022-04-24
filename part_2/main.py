47098427e20372d367ceeccfd60db76f96bb1541
Andrey <21elektro87@gmail.com>
2022-04-24 08:17:56 +0600

    create Home work №11

--- part_2/main.py
+++ part_2/main.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 from utils import *
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def load_():
-    candidates_list = get_candidates("candidates.json")
-    return candidates_list
+def all_candidates():
+    candidates = load_candidates_from_json("candidates.json")
+    return render_template('list.html', candidates=candidates)
+
+
+@app.route("/candidate/<int:candidate_id>")
+def selected_candidate(candidate_id):
+    candidate = candidate_selection_by_id(candidate_id)
+    return render_template('card.html', candidate=candidate)
+
+
+@app.route("/search/<candidate_name>")
+def candidates_by_name(candidate_name):
+    list_by_name = candidate_selection_by_name(candidate_name)
+    return render_template('search.html', list_by_name=list_by_name)
+
+
+@app.route("/skill/<skill_name>")
+def candidates_by_skill(skill_name):
+    list_by_skill = candidate_selection_by_skill(skill_name)
+    return render_template('skill.html', list_by_skill=list_by_skill, skill_name=skill_name.title())
+
+
+app.run(debug=True, port=5005)
--- part_2/utils.py
+++ part_2/utils.py
@@ -1,42 +1,43 @@
 import json
 
 
-# - `load_candidates_from_json(path)` – возвращает список всех кандидатов
-#
-# - `get_candidate(candidate_id)` – возвращает одного кандидата по его id
-#
-# - `get_candidates_by_name(candidate_name)` – возвращает кандидатов по имени
-#
-# - `get_candidates_by_skill(skill_name)` – возвращает кандидатов по навыку
-
-def get_candidates(path):
+def load_candidates_from_json(path):
     with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
-def formatted_candidates(candidates_list):
-    result = '<pre>'
-    for candidate in candidates_list:
-        result += (
-            f'<h3>Имя кандидата - <mark><em>{candidate["name"]}</em></mark></h3>'
-            f'Позиция кандидата - {candidate["position"]}<br>'
-            f'Навыки через запятую - {candidate["skills"]}<br>'
-            f'Ссыль на профиль - <a  href="/candidates/{candidate["id"]}">Profile</a><br><br>'
-        )
-    result += '<pre>'
-    return result
+# (?)преобразует исходный список словарей в словарь, где ключ - ИД, значение - экземпляр класса Person
+# def formatted_candidates(candidates_list):
+#     basa_candidasa = {}
+#     for candidate in candidates_list:
+#         person = Person(candidate["id"], candidate["name"], candidate["picture"], candidate["position"],
+#                         candidate["gender"], candidate["age"], candidate["skills"])
+#         basa_candidasa[candidate["id"]] = person
+#     return basa_candidasa
 
 
-def candidate_selection_by_id(candidates_list, candidate_id):
+def candidate_selection_by_id(candidate_id):
+    candidates_list = load_candidates_from_json("candidates.json")
     for candidate in candidates_list:
         if candidate['id'] == candidate_id:
             return candidate
 
 
-def candidate_selection_by_skill(candidates_list, candidate_skill):
+def candidate_selection_by_name(candidate_name):
+    candidates_list = load_candidates_from_json("candidates.json")
+    result = []
+    for candidate in candidates_list:
+        candidate_names_list = candidate['name'].lower().split(' ')
+        if candidate_name.lower() in candidate_names_list:
+            result.append(candidate)
+    return result
+
+
+def candidate_selection_by_skill(candidate_skill):
+    candidates_list = load_candidates_from_json("candidates.json")
     result = []
     for candidate in candidates_list:
         candidate_skills = candidate['skills'].lower().split(', ')
-        if candidate_skill in candidate_skills:
+        if candidate_skill.lower() in candidate_skills:
             result.append(candidate)
     return result
